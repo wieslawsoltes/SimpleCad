aa661e3fa5da19f212869639d7c0ec0197ab773e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-03-21 09:22:52 +0100

    Update DxfBlock.cs

--- SimpleCad/Model/Dxf/Entities/DxfBlock.cs
+++ SimpleCad/Model/Dxf/Entities/DxfBlock.cs
@@ -12,11 +12,25 @@ public class DxfBlock : DxfEntity
     public override void Render(SKCanvas context, double zoomFactor)
     {
         // TODO:
+        foreach (var child in Children)
+        {
+            if (child is DxfEntity entity)
+            {
+                entity.Render(context, zoomFactor);
+            }
+        }
     }
 
     public override void Invalidate()
     {
         // TODO:
+        foreach (var child in Children)
+        {
+            if (child is DxfEntity entity)
+            {
+                entity.Invalidate();
+            }
+        }
     }
 
     public override bool Contains(float x, float y)
